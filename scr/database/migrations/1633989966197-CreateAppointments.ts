4d3acc041122eacd672d9012f669aadfb4c99552
Henrique <inaaldo.henrique@gmail.com>
2021-10-11 19:42:40 -0300

    environment configuration

---
+++ scr/database/migrations/1633989966197-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1633989966197 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
