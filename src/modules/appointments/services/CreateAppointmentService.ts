9329dd0dac79bf6d8314c5c95d436381807f4654
Henrique <inaaldo.henrique@gmail.com>
2021-10-25 21:30:55 -0300

    update service create appointment

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore } from 'date-fns';
+import getHours from 'date-fns/getHours';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -14,6 +15,18 @@ class CreateAppointmentService {
 
   public async execute({ date, user_id, provider_id }: Request): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if(isBefore(appointmentDate, Date.now())){
+      throw new Error('Você não pode criar um agendamento em data passada')
+    }
+
+    if(user_id === provider_id){
+      throw new Error('Você não pode criar um agendamento com você mesmo')
+    }
+
+    if(getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17){
+      throw new Error('Você só pode criar agendamento das 8:00 às 17:00git ')
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
